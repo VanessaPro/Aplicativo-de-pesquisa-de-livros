fa5f5340a75cb8bb3dc000694e7b1d3e80762288
Vanessa Santos <114835013+VanessaPro@users.noreply.github.com>
2023-06-24 13:47:54 -0300

    perf

--- pesquisa/src/Components/Booklist/Booklist.js
+++ pesquisa/src/Components/Booklist/Booklist.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import { useGlobalContext } from '../../context';
+import Book from "../Book/BookList";
+import Loading from "../..Loader/Loader";
+import coverImg from "../..img/Captura de tela 2023-06-20 152822.jpg";
+import "./Booklist.css";
 
-const Booklist= () =>{
-    return(
-        <div>
-          Booklist
+//https://covers.openlibrary.org/b/id/240727-S.jpg
+
+const BookList = () => {
+  const {books, loading, resultTitle} = useGlobalContext();
+  const booksWithCovers = books.map((singleBook) => {
+    return {
+      ...singleBook,
+      
+      id: (singleBook.id).replace("/works/", ""),
+      cover_img: singleBook.cover_id ? `https://covers.openlibrary.org/b/id/${singleBook.cover_id}-L.jpg` : coverImg
+    }
+  });
+
+  if(loading) return <Loading />;
+
+  return (
+    <section className='booklist'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>{resultTitle}</h2>
         </div>
-    )
+        <div className='booklist-content grid'>
+          {
+            booksWithCovers.slice(0, 30).map((item, index) => {
+              return (
+                <Book key = {index} {...item} />
+              )
+            })
+          }
+        </div>
+      </div>
+    </section>
+  )
 }
 
-export default Booklist
+export default BookList
