d1141de9af7f7ba4d6ae3d0365be9e31db742f42
Vanessa Santos <114835013+VanessaPro@users.noreply.github.com>
2023-06-28 14:18:54 -0300

    fix

--- pesquisa/src/Components/BookDetails/BookDetails.jsx
+++ pesquisa/src/Components/BookDetails/BookDetails.jsx
@@ -25,7 +25,7 @@ const BookDetails = () => {
         if(data){
           const {description, title, covers, subject_places, subject_times, subjects} = data;
           const newBook = {
-            description: description ? description.value : "No description found",
+            description: description ? description.value: "No description found",
             title: title,
             cover_img: covers ? `https://covers.openlibrary.org/b/id/${covers[0]}-L.jpg` : coverImg,
             subject_places: subject_places ? subject_places.join(", ") : "No subject places found",
